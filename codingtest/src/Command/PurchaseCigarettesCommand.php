b8ab57440d035cb8e1e39a82640136be7e31c4b0
djogost <stefanpirlo21@gmail.com>
2020-04-27 16:12:03 +0200

    Minor fix

--- codingtest/src/Command/PurchaseCigarettesCommand.php
+++ codingtest/src/Command/PurchaseCigarettesCommand.php
@@ -33,7 +33,7 @@ class PurchaseCigarettesCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $itemCount = (int)$input->getArgument('packs');
-        $amount = (float)\str_replace(',', '.', $input->getArgument('amount'));
+        $amount = (float)str_replace(',', '.', $input->getArgument('amount'));
 
         $coins = array(500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01);
 
